e19ead478f56e178abe10c2c9a7980d544f736b9
Reini Urban <rurban@cpan.org>
2022-02-24 18:17:14 +0100

    improve fnv3
    only 1-pass, with different starts

--- fnv3.h
+++ fnv3.h
@@ -4,21 +4,18 @@
 static inline void fnv3(const void *key, size_t len, uint64_t seed,
 			uint64_t *hashes)
 {
-  uint64_t  h = seed;
+  uint64_t  h0 = seed ^ UINT64_C(0xcbf29ce484222325);
+  uint64_t  h1 = seed ^ UINT64_C(0xc4ceb9fe1a85ec53); /* from PMP_Multilinear */
   uint8_t  *data = (uint8_t *)key;
   const uint8_t *const end = &data[len];
 
-  h ^= UINT64_C(0xcbf29ce484222325);
   while (data < end) {
-    h ^= *data++;
-    h *= UINT64_C(0x100000001b3);
+    h0 ^= *data;
+    h0 *= UINT64_C(0x100000001b3);
+    h1 ^= *data;
+    h1 *= UINT64_C(0x100000001b3);
+    data++;
   }
-  hashes[0] = h;
-
-  data = (uint8_t *)key;
-  while (data < end) {
-    h ^= *data++;
-    h *= UINT64_C(0x100000001b3);
-  }
-  hashes[1] = h;
+  hashes[0] = h0;
+  hashes[1] = h1;
 }
