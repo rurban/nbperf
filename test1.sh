f9b3b007cca0abb5c82aa13f3658784ea43aac0a
Reini Urban <rurban@cpan.org>
2022-07-06 06:24:09 +0200

    test1.sh use getopts
    for -h hash and more sanity

--- test1.sh
+++ test1.sh
@@ -1,42 +1,61 @@
 #!/bin/sh
-# ./test1 chm [big] [int]
 if [ -z $CFLAGS ]; then
     CFLAGS="-g -march=native -fno-strict-aliasing -Wall -Wextra"
 else
     make clean
 fi
 
-if [ -z "$1" ]; then
-    alg=chm
-    out=$alg
-    IN=_words
-else
-    alg=$1
-    out=$alg
-    if [ $alg != chm -a $alg != chm3 -a $alg != bdz ]; then
-        echo Invalid alg=$alg
-        exit 1
-    fi
-    if [ -z "$2" ]; then
-        IN=_words1000
-    elif [ x$2 = xbig ]; then
-        IN=_words
-        out=big${alg}
-    elif [ x$2 = xint ]; then
-        opts="-I"
-        copts="-D_INTKEYS"
+usage() { echo "Usage: $0 [-a alg] [-h hash] [-b] [-Ifps]" 1>&2; exit 1; }
+
+alg=chm
+IN=_words1000
+hashc=mi_vector_hash.c
+while getopts "a:h:bIfps" o; do
+    case "$o" in
+        a) alg=$OPTARG
+           if [ $alg != chm -a $alg != chm3 -a $alg != bdz ]; then
+               echo Invalid -a $alg
+               usage
+           fi
+           ;;
+        h) hash=$OPTARG
+           opts="$opts -h $hash"
+           if [ x$hash != xmi_vector_hash ]; then unset hashc; fi
+           ;;
+        b) big=1 ;;
+        I) intkeys=1
+           unset hashc
+           opts="$opts -I"
+           copts="-D_INTKEYS"
+           ;;
+        f) opts="$opts -f" ;;
+        s) opts="$opts -s" ;;
+        *) usage ;;
+    esac
+done
+shift $((OPTIND-1))
+out=$alg
+
+if [ -n "$intkeys" ]; then
+    [ -z $hash ] || usage
+    if [ -n "$big" ]; then
+        IN=_randbig
+        out=intbig${alg}
+    else
         IN=_rand100
         out=int${alg}
     fi
-    if [ x$3 = xint ]; then
-        opts="-I"
-        copts="-D_INTKEYS"
-        IN=_randbig
-        out=intbig${alg}
+else
+    if [ -n "$big" ]; then
+        IN=_words
+        out=big${alg}
     fi
-    if [ $alg = bdz ]; then
-        opts="$opts -m $IN.map"
-    fi
+fi
+if [ x$alg = xbdz ]; then
+    opts="$opts -m $IN.map"
+fi
+if [ -n "$hash" ]; then
+    out="${out}_${hash}"
 fi
 
 echo make CFLAGS="\"${CFLAGS}\"" nbperf ${IN}
@@ -45,8 +64,8 @@ test -e ${IN} || exit 1
 
 echo ./nbperf -p $opts -a $alg -o _test_${out}.c $IN
 ./nbperf -p $opts -a $alg -o _test_${out}.c $IN
-echo cc $CFLAGS -I. -D$alg $copts _test_${out}.c test_main.c mi_vector_hash.c -o _test_${out}
-cc $CFLAGS -I. -D$alg $copts _test_${out}.c test_main.c mi_vector_hash.c -o _test_${out}
+echo cc $CFLAGS -I. -D$alg $copts _test_${out}.c test_main.c $hashc -o _test_${out}
+cc $CFLAGS -I. -D$alg $copts _test_${out}.c test_main.c $hashc -o _test_${out}
 echo ./_test_${out} $IN
 ./_test_${out} $IN
 
