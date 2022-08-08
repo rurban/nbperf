892d2ac861c60a250ff784bfe36e250066df4fa8
Reini Urban <rurban@cpan.org>
2022-08-08 10:59:38 +0200

    Update perf --c (-c -2) graphs

---
+++ perf_img_c2.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# requires plotly
+basedir="$(dirname $0)"
+VERSION="rurban/nbperf `cat VERSION`"
+if test -z "$PNG"; then
+  PNG="size nbperf run"
+fi
+
+# PATH="~/.local/lib/python3.10/site-packages/kaleido/executable:$PATH"
+
+perf_graph()
+{
+    LOG=$1
+    TITLE=$2
+    python3 ${basedir}/perf_plot.py "$LOG" "$TITLE"
+}
+
+size() {
+  perf_graph \
+    'size-c-2.log' \
+    "sizes ($VERSION)"
+}
+
+nbperf() {
+  perf_graph \
+    'nbperf-c-2.log' \
+    "compile-times ($VERSION)"
+}
+
+run() {
+  perf_graph \
+    'run-c-2.log' \
+    "run-times ($VERSION)"
+}
+
+for png in $PNG; do
+    $png
+    test -d ../doc && mv $png-c-2.png $png-c-2.svg ../doc/
+done
